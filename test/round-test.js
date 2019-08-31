b6f0b2ac15c137eae51cda0d70af6e6c3516799f
Roger Hughes <Rogerallenhughes@hotmail.com>
2019-08-31 15:37:28 -0600

    Add testing suite to Round class Co-Authored-By: Ayla Dharamsey <ayla.dharamsey@gmail.com>, Co-Authored-By: Colin Koga <colin.koga@gmail.com>, Co-Authored-By: Roger Hughes <rogerallenhughes@hotmail.com>, Co-Authored-By: Kirk Veitch <kirk.a.veitch@gmail.com>

---
+++ test/round-test.js
@@ -0,0 +1,40 @@
+import chai from 'chai';
+const expect = chai.expect;
+const data = require('./survey-sample-data');
+const Player = require('../src/player');
+const Round = require('../src/round');
+const Turn = require('../src/turn');
+
+
+let 
+player1, 
+player2, 
+round,
+turn;
+
+describe('ROUND CLASS', function() {
+  beforeEach(() => { 
+    player1 = new Player('Kirk', 1);
+    player2 = new Player('Ayla', 2);
+    round = new Round( );
+    turn = new Turn(round.getSurveyAnswers(), player1.name, player2.name);
+  });
+
+  it('Should have a single survey', function() {
+  	expect(round.survey).to.be.typeof('array')
+  });
+
+  it('Should create its own turns', function() {
+  	expect(turn.player1).to.equal('Kirk');
+  	round.makeNewTurn();
+  	expect(turn.player1).to.equal('?');
+  });
+
+  it('Should know how many answers are left', function() {
+
+  });
+
+  it('Should declare a winner when all correct answers are guessed', function() {
+
+  });
+};
