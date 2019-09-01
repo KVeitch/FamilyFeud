a1b1f0e9e37ad53f66b22f13e242b795673bbcdb
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-09-01 15:19:40 -0600

    Append names to dom and update score to 0 initially
    Co-Authored-By: Ayla Dharamsey <ayla.dharamsey@gmail.com>,
    Co-Authored-By: Colin Koga <colin.koga@gmail.com>,
    Co-Authored-By: Roger Hughes <rogerallenhughes@hotmail.com>,
    Co-Authored-By: Kirk Veitch <kirk.a.veitch@gmail.com>

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,11 +2,22 @@ const Game = require('./game');
 const data = require('../test/survey-sample-data');
 const game = new Game(data);
 
-$('.player__button').click(function () {
-  game.makePlayers($('.player__input1').val(), $('.player__input2').val())
-  $('.main__player').toggle('hidden')
-})
+$('.player__button').click(playerButtonHelper);
 
+function createPlayers() {
+  game.makePlayers($('.player__input1').val(), $('.player__input2').val());
+}
+
+function appendNames() {
+  $('.jq-name1').text(game.player1.name);
+  $('.jq-name2').text(game.player2.name);
+}
+
+function playerButtonHelper() {
+  $('.main__player').toggle('hidden');
+  createPlayers();
+  appendNames();
+}
 
 
 
