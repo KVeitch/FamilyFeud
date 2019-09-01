d160bfb16e662a53a2ac769a849759f01aa03bba
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-09-01 10:48:52 -0600

    Removed .only and corrected typos
    Co-Authored-By: Colin Koga <colin.koga@gmail.com>

--- test/player-test.js
+++ test/player-test.js
@@ -9,17 +9,17 @@ describe('PLAYER CLASS', function() {
     player2 = new Player('Mildred', 2 );
   })
 
-  it.only('should have a name', function() {
+  it('should have a name', function() {
     expect(player1.name).to.equal('Tina');
     expect(player2.name).to.equal('Mildred');
   });
 
-  it.only('should have an initial score of 0', function() {
+  it('should have an initial score of 0', function() {
     expect(player1.score).to.equal(0);
     expect(player2.score).to.equal(0);
   });
 
-  it.only('should be able to update its score', function() {
+  it('should be able to update its score', function() {
     expect(player2.score).to.equal(0);
     player2.updateScore(35)
     expect(player2.score).to.equal(35);
