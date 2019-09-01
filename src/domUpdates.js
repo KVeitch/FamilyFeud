55d397ba9200517b75890fb19af585b8f4bf34f4
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-09-01 13:31:06 -0600

    player button now toggles hidden attribute Co-Authored-By: Colin Koga <colin.koga@gmail.com>, Co-Authored-By: Roger Hughes <rogerallenhughes@hotmail.com>, Co-Authored-By: Kirk Veitch <kirk.a.veitch@gmail.com>

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,4 +1,12 @@
-$('.jq-player1').text('Molly');
+const Game = require('./game');
+const data = require('../test/survey-sample-data');
+const game = new Game(data);
+
+$('.player__button').click(function () {
+  game.makePlayers($('.player__input1').val(), $('.player__input2').val())
+  $('.main__player').toggle('hidden')
+})
+
 
 
 
