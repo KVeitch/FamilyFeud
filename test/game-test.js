b5175599452290fa075ed52d319e6f8865886db7
Colin Turing <colin.koga@gmail.com>
2019-09-10 11:48:47 -0600

    Refactor testing files delete console logs REMed out code and spacing

--- test/game-test.js
+++ test/game-test.js
@@ -1,19 +1,11 @@
-
 import chai from 'chai';
 import domUpdates from '../src/domUpdates';
 const expect = chai.expect;
 const spies = require('chai-spies');
-// const data = require('./survey-sample-data');
 import data from './sample-data-3surveys';
-// const Game = require('../src/game');
-// const Player = require('../src/player');
-// const Round = require('../src/round');
-// const Turn = require('../src/turn');
 import Game from '../src/game';
 import Player from '../src/player'
 import Round from '../src/round';
-// import FastMoney from '../src/fastMoneyRound'  **Currently not using in game testing might delete later
-// import Turn from '../src/turn'; **Currently not using in game testing might delete later
 chai.use(spies);
 chai.spy.on(domUpdates, ['appendNames'], () => {});
 
@@ -21,16 +13,12 @@ let
 game,
 player1, 
 player2;
-// turn;
 
 describe('GAME CLASS', function() {
   beforeEach(() => { 
     game = new Game(data)
     player1 = new Player('Kirk');
     player2 = new Player('Ayla');
-    // round = new Round(game.surveys[0], player1.name, player2.name);
-    // turn = new Turn(round.survey.answers, round.currentPlayer);
-    // console.log(turn)
   });
   
   it('Should start with no surveys', function() {
@@ -55,10 +43,6 @@ describe('GAME CLASS', function() {
     game.makePlayers('Kirk', 'Ayla')
     game.getSurveys();
     game.startRound();
-    console.log('ROUND! ',Round)
-
-    // expect(game.round).to.be.an.instanceOf(Round);
-    console.log("hi: ", game.round instanceof Round)
     expect(game.round).to.be.an.instanceof(Round)
   });
   
--- test/turn-test.js
+++ test/turn-test.js
@@ -2,45 +2,30 @@ import chai from 'chai';
 const expect = chai.expect;
 const spies = require('chai-spies');
 import data from './sample-data-3surveys';
-// import $ from 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js'
-// const data = require('./test/sample-data-3survey');
 import Game from '../src/game';
-import Player from '../src/player'
-import Round from '../src/round';
 import Turn from '../src/turn';
 import domUpdates from '../src/domUpdates';
-chai.use(spies)
-// chai.spy.on(Turn, 'hasAnswer', () => {})
+chai.use(spies);
 
-// chai.spy.on(domUpdates, ['appendSurvey', 'hideSplashPage', 'appendAnswers', 'revealAnswers', 'badFeedback', 'goodFeedback', 'removeFeedback', 'postScore', 'clearGuessInput', 'togglePlayerDisplays', 'increaseScore'], () => { });
-// const Game = require('../src/game');
-// const Player = require('../src/player');
-// const Round = require('../src/round');
-// const Turn = require('../src/turn');
-
-
-let game, player1, player2, round, turn;
+let game, round, turn;
 
 describe('TURN CLASS', function() {
   beforeEach(() => {
-    chai.spy.on(Turn, ['hasAnswer'], () => {})
+    chai.spy.on(Turn, ['hasAnswer', 'giveFeedback'], () => true)
     chai.spy.on(domUpdates, [''])
     global.$ = require('jquery');
     global.window = window;
     game = new Game(data);
     game.getSurveys();
-    // player1 = new Player('Kirk');
-    // player2 = new Player('Ayla');
     game.startRound();
     game.makePlayers('Kirk', 'Ayla');
     game.round.makeNewTurn();
-    
+
     
     afterEach(() => {
       chai.spy.restore(domUpdates);
     });
-    // round = new Round(game.surveys[0]);
-    // turn = new Turn(round.survey.answers, round.currentPlayer);
+
   })
   
   describe('hasAnswer', () => {
@@ -49,13 +34,10 @@ describe('TURN CLASS', function() {
       game.round.turn.hasAnswer(round);
       expect(game.round.turn.hasAnswer).to.have.been.called(1);
       expect(game.round.turn.hasAnswer).to.to.have.been.called(2);
-
-      // $('.player__input').should.have.prop('maxlength');
-      // expect($('.player__input')).to.have.prop('maxlenght', 25)
     });
   })
   
-  describe('.giveFeedback', function() {
+  describe('giveFeedback', function() {
     it('should let player know if they were right or wrong', function() {
       expect(turn.giveFeedback(boo)).to.equal('feedback');
       expect(turn.giveFeedback(boo2)).to.equal('feedback');
