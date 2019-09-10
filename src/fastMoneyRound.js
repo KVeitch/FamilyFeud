fa5f1d29167debfea8c9f70bc008c593ff1ba594
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-09-09 18:56:39 -0600

    clean fastMoneyRound again

--- src/fastMoneyRound.js
+++ src/fastMoneyRound.js
@@ -16,7 +16,7 @@ class FastMoneyRound extends Round {
       if (timeLeft == 0) {
         clearTimeout(game.round.timerId);
         game.round.removeTimerText();
-        game.roundCount === 4 ? domUpdates.displayGameWinner(game.round.getGameWinner(game)) : (startRound3or4(), hideAnswers()) ;
+        game.roundCount === 4 ? domUpdates.displayGameWinner(game.round.getGameWinner(game)) : (startRound3or4(), hideAnswers());
       } else {
         $('.container__round--timer').text(timeLeft + ' seconds remaining');
         timeLeft--;
@@ -32,17 +32,9 @@ class FastMoneyRound extends Round {
     window.stopTimer = stopTimer; 
   }
 
-  // fastRoundTimeout(game) {
-  //     domUpdates.hideAnswers();
-  //     game.startFastRound();
-  //     domUpdates.appendNames(game)
-  //     game.round.playerTimeOut();
-  //     domUpdates.setFastRoundHeader();
-  // }
-
   removeTimerText() {
     $('.container__round--timer').text('')
-  } // needs to go to domUpdates
+  } 
 
   assignMultiplier(multiplier) {
     this.multiplier = multiplier;
